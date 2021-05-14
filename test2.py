887e6bac1c40ea6c0bc2f5593a57e6b1880776b0
neri0000 <72220347+neri0000@users.noreply.github.com>
2021-05-15 04:02:56 +0900

    がったい

--- test2.py
+++ test2.py
@@ -3,12 +3,25 @@ import schedule
 import time
 import random
 import datetime
+import urllib3
+from bs4 import BeautifulSoup
+
+url = 'https://weather.yahoo.co.jp/weather/jp/13/4410.html'
+
+#URLにアクセスする 戻り値にはアクセスした結果やHTMLなどが入ったinstanceが帰ってきます
+http = urllib3.PoolManager()
+instance = http.request('GET', url)
+#instanceからHTMLを取り出して、BeautifulSoupで扱えるようにパースします
+soup = BeautifulSoup(instance.data, 'html.parser')
+tenki_today = soup.select_one('#main > div.forecastCity > table > tr > td > div > p.pict')
+
 
 nia = time.time()
 
-tolk_list = ["軽く机回りを掃除したらどうだ？効率が落ちるど","課題は終わっているか？早めにやっといた方が...終わってる？アッハイスミマセンでした",
-"猫背になってないか？肘をのばして体を伸ばしてあげたらどうだ？","捗っているか？君は頑張り屋だからな、適度に休憩を挟めよ","そろそろ休憩したい。","もしかして煩いか？...ちょっと黙るわ",
-"そろそろアレ買っといた方がいいんじゃない？","スマホ触ってないだろうな？メリハリ持てているか？","大好きだぜ～"]
+tolk_list = ["軽く机回りを掃除したらどうだ？作業スペースが狭いと効率が落ちるど","課題は終わっているか？早めにやっといた方が...え、終わってる？余計なお世話だったか",
+"猫背になってないか？体を伸ばしてあげたらどうだ？","捗っているか？君は頑張り屋だからな、適度に休憩を挟めよ","そろそろ休憩したい。","もしかして煩いか？",
+"そろそろアレ買っといた方がいいんじゃない？","スマホ触ってないだろうな？メリハリ持てているか？","お疲れ様！どう？ここらで一旦休憩と行きましょうよ...あっ保存忘れずにね",
+"そんでもって今日の天気は"+tenki_today.text+"らしいぞ！窓の外見て確認してみてよ...どうだった？"]
 
 act = random.randint(0,len(tolk_list)-1)
 
